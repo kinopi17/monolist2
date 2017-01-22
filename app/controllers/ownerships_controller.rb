62f395dbb51cc25928b9a292d779ec610e6e89c4
Yasuyuki Kinoshita <kinoko0618@gmail.com>
2017-01-22 05:02:00 +0000

    submit

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -9,32 +9,38 @@ class OwnershipsController < ApplicationController
     end
 
     # itemsテーブルに存在しない場合は楽天のデータを登録する。
-    if @item.new_record?
-      # TODO 商品情報の取得 RakutenWebService::Ichiba::Item.search を用いてください
-      items = {}
-
-      item                  = items.first
-      @item.title           = item['itemName']
-      @item.small_image     = item['smallImageUrls'].first['imageUrl']
-      @item.medium_image    = item['mediumImageUrls'].first['imageUrl']
-      @item.large_image     = item['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
-      @item.detail_page_url = item['itemUrl']
-      @item.save!
-    end
-
+  　if @item.new_record?
+    items = RakutenWebService::Ichiba::Item.search(item_code: params[:item_code])
+    item                  = items.first
+    @item.title           = item['itemName']
+    @item.small_image     = item['smallImageUrls'].first['imageUrl']
+    @item.medium_image    = item['mediumImageUrls'].first['imageUrl']
+    @item.large_image     = item['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
+    @item.detail_page_url = item['itemUrl']
+    @item.save!
+  end
+  
     # TODO ユーザにwant or haveを設定する
     # params[:type]の値にHaveボタンが押された時には「Have」,
     # Wantボタンが押された時には「Want」が設定されています。
-    
-
-  end
+  def create    
+    if params[:type]=="Have"
+    current_user.have(@item)
+    elsif params[:type]=="Want"
+    current_user.want(@item)
+    end
+  end  
 
   def destroy
-    @item = Item.find(params[:item_id])
+    if params[:type]=="Have it"
+    current_user.unhave(@item)
+    elsif params[:type]=="Want it"
+    current_user.unwant(@item)
+    end
+  end
+end  
 
     # TODO 紐付けの解除。 
     # params[:type]の値にHave itボタンが押された時には「Have」,
     # Want itボタンが押された時には「Want」が設定されています。
 
-  end
-end
